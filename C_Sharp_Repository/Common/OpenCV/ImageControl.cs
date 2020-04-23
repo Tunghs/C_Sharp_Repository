00d52bff6616a4f72bffb996a724c624fa3dcdb6
이성현 <dljdgus1211@naver.com>
2020-04-23 14:45:44 +0900

    add code

---
+++ C_Sharp_Repository/Common/OpenCV/ImageControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenCvSharp;
+
+namespace Common.OpenCV
+{
+    public class ImageControl
+    {
+        public Mat RoiCrop(string path, int x, int y, int roiWidth, int roiHeight)
+        {
+            try
+            {
+                Mat image = Cv2.ImRead(path, ImreadModes.Unchanged);
+                Rect rect = new Rect(x, y, roiWidth, roiHeight);
+
+                Mat dst = image.SubMat(rect);
+
+                return dst;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public void GridCrop()
+        {
+
+        }
+    }
+}
---
+++ C_Sharp_Repository/Common/OpenCV/ImageConvert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenCvSharp;
+
+namespace Common.OpenCV
+{
+    public class ImageConvert
+    {
+        public ImageConvert()
+        {
+
+        }
+
+        public unsafe Mat ChangePixelValue(string path, int srcPixelValue, int trgPixelValue)
+        {
+            Mat image = Cv2.ImRead(path, ImreadModes.Unchanged);
+
+            byte* data = (byte*)image.DataPointer;
+            for (int y = 0; y < image.Rows; ++y)
+            {
+                for (int x = 0; x < image.Cols; ++x)
+                {
+                    if (data[y * image.Step() + x * image.ElemSize()] == (byte)srcPixelValue)
+                        data[y * image.Step() + x * image.ElemSize()] = (byte)trgPixelValue;
+                }
+            }
+
+            return image;
+        }
+    }
+}
